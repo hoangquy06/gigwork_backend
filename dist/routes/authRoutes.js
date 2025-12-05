8a3e05e8bbfeb8b340c71d42010f050c1d64f932
hoangquy06 <23521322@gm.uit.edu.vn>
2025-12-06 01:50:02 +0700

    Delete dist directory

--- dist/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const express = require('express');
-const ctrl = require('../controllers/authController');
-const { asyncHandler } = require('../middleware/asyncHandler');
-const router = express.Router();
-router.post('/api/auth/register', asyncHandler(ctrl.register));
-router.post('/api/auth/login', asyncHandler(ctrl.login));
-module.exports = router;
-//# sourceMappingURL=authRoutes.js.map
