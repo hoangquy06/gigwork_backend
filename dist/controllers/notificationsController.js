180a126e9da69afd6193342e8241a7736c425328
hoangquy06 <23521322@gm.uit.edu.vn>
2025-12-09 07:54:37 +0700

    Delete dist directory

--- dist/controllers/notificationsController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const Notifications = require('../models/Notifications');
-async function list(req, res) {
-    const out = await Notifications.list(req.user.id, req.query || {});
-    return res.status(200).json(out);
-}
-module.exports = { list };
-//# sourceMappingURL=notificationsController.js.map
